ff2989ab26e2f6b33dfd8a8878605a6765ee45d3
NoobLk <125950957+nooblk-98@users.noreply.github.com>
2026-01-27 18:31:48 +0530

    gg

--- scripts/sync/sync-wrtbwmon.sh
+++ scripts/sync/sync-wrtbwmon.sh
@@ -6,7 +6,7 @@ set -e
 # Environment parameters for luci-app-wrtbwmon
 export SYNC_REPO_URL="https://github.com/brvphoenix/luci-app-wrtbwmon.git"
 export SYNC_REMOTE_PATH="."
-export SYNC_DEST_DIR="feeds"
+export SYNC_DEST_DIR="feeds/wrtbwmon"
 export SYNC_COPY_SUBDIRS=true
 export SYNC_CLEAN_DEST=true
 
