97b9cf14ecf5124b50f07cade763bf30cc1000f2
Lahiru Liyanage <liyanagelsofficial@gmail.com>
2026-01-27 13:52:07 +0530

    Update sync-internet-detector.sh

--- scripts/sync/sync-internet-detector.sh
+++ scripts/sync/sync-internet-detector.sh
@@ -3,18 +3,48 @@
 
 set -e
 
-# Environment parameters for luci-app-internet-detector
+
 export SYNC_REPO_URL="https://github.com/gSpotx2f/luci-app-internet-detector.git"
-export SYNC_REMOTE_PATH="."
-export SYNC_DEST_DIR="packages/internet-detector"
+export SYNC_REMOTE_PATH="/luci-app-internet-detector"
+export SYNC_DEST_DIR="packages/luci-app-internet-detector"
 export SYNC_COPY_SUBDIRS=true
-export SYNC_CLEAN_DEST=false
+export SYNC_CLEAN_DEST=true
 
 # Call the generic sync script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 bash "${SCRIPT_DIR}/sync-repo.sh"
 
-# Environment parameters for luci-app-internet-detector
+export SYNC_REPO_URL="https://github.com/gSpotx2f/luci-app-internet-detector.git"
+export SYNC_REMOTE_PATH="/internet-detector-mod-telegram"
+export SYNC_DEST_DIR="packages/internet-detector-mod-telegram"
+export SYNC_COPY_SUBDIRS=true
+export SYNC_CLEAN_DEST=true
+
+# Call the generic sync script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+bash "${SCRIPT_DIR}/sync-repo.sh"
+
+export SYNC_REPO_URL="https://github.com/gSpotx2f/luci-app-internet-detector.git"
+export SYNC_REMOTE_PATH="/internet-detector-mod-modem-restart"
+export SYNC_DEST_DIR="packages/internet-detector-mod-modem-restart"
+export SYNC_COPY_SUBDIRS=true
+export SYNC_CLEAN_DEST=true
+
+# Call the generic sync script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+bash "${SCRIPT_DIR}/sync-repo.sh"
+
+export SYNC_REPO_URL="https://github.com/gSpotx2f/luci-app-internet-detector.git"
+export SYNC_REMOTE_PATH="/internet-detector-mod-email"
+export SYNC_DEST_DIR="packages/internet-detector-mod-email"
+export SYNC_COPY_SUBDIRS=true
+export SYNC_CLEAN_DEST=true
+
+# Call the generic sync script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+bash "${SCRIPT_DIR}/sync-repo.sh"
+
+
 export SYNC_REPO_URL="https://github.com/gSpotx2f/luci-app-internet-detector.git"
 export SYNC_REMOTE_PATH="/internet-detector"
 export SYNC_DEST_DIR="packages/dependencies/internet-detector"
