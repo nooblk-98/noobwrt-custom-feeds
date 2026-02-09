0c9fea14aa228fbfde082a9c80a18e2aa1688af5
Lahiru Liyanage <liyanagelsofficial@gmail.com>
2026-02-09 12:48:27 +0530

    Remove luci-app-netspeedtest package
    Delete the entire package/luci-app-netspeedtest directory and its contents (Makefile, LICENSE, README, UI JS files, images, PO translations, init scripts, root files, and related resources). Also update scripts/sync/sync-luci-app-netspeedtest.sh to reflect the package removal.

--- scripts/sync/sync-luci-app-netspeedtest.sh
+++ scripts/sync/sync-luci-app-netspeedtest.sh
@@ -4,9 +4,9 @@
 set -e
 
 # Environment parameters for luci-app-wrtbwmon
-export SYNC_REPO_URL="https://github.com/muink/luci-app-netspeedtest.git"
+export SYNC_REPO_URL="https://github.com/sirpdboy/luci-app-netspeedtest.git"
 export SYNC_REMOTE_PATH="."
-export SYNC_DEST_DIR="package/luci-app-netspeedtest"
+export SYNC_DEST_DIR="feeds/luci-app-netspeedtest"
 export SYNC_COPY_SUBDIRS=true
 export SYNC_CLEAN_DEST=true
 
