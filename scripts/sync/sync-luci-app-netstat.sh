5b5b8a8c1df498e37338467d292fa361b516e78b
Lahiru Liyanage <liyanagelsofficial@gmail.com>
2026-02-11 11:07:35 +0530

    Update sync-luci-app-netstat.sh

--- scripts/sync/sync-luci-app-netstat.sh
+++ scripts/sync/sync-luci-app-netstat.sh
@@ -6,7 +6,7 @@ export SYNC_REPO_URL="https://github.com/nooblk-98/luci-app-netstat.git"
 export SYNC_REMOTE_PATH="."
 export SYNC_DEST_DIR="package/luci-app-netstat"
 export SYNC_COPY_SUBDIRS=true
-export SYNC_CLEAN_DEST=true
+export SYNC_CLEAN_DEST=false
 
 # Call the generic sync script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
